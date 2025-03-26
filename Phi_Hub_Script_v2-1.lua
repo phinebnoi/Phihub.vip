96c90b51d68c35e0ecc8b54398e8d3b811d3ddd1
phinebnoi <ngocphilop5b@gmail.com>
2025-03-26 19:29:32 +0700

    Add files via upload

---
+++ Phi_Hub_Script_v2-1.lua
@@ -0,0 +1,32 @@
+
+-- Phi Hub Script
+-- This script includes all functionalities: Farm, Random Fruit, Random Bone, Auto Haki, and more.
+
+-- Function to perform auto farming
+function AutoFarm()
+    -- Your farming code goes here.
+end
+
+-- Function to find and randomize fruit
+function RandomFruit()
+    -- Your random fruit finding code goes here.
+end
+
+-- Function to randomize bones
+function RandomBone()
+    -- Your random bone code goes here.
+end
+
+-- Function to use Haki
+function UseHaki()
+    -- Your Haki activation code goes here.
+end
+
+-- Main execution block to run all functions
+while true do
+    AutoFarm()
+    RandomFruit()
+    RandomBone()
+    UseHaki()
+    wait(1) -- Wait for 1 second before repeating the loop
+end
